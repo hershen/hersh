5eced20922fbf29f072942499a9386849e9a7f40
Alon Hershenhorn <hershen@gmail.com>
2021-09-09 16:29:39 -0700

    More tree-sitter-wrapper

--- src/tree-sitter-wrapper.hpp
+++ src/tree-sitter-wrapper.hpp
@@ -2,33 +2,53 @@
 
 #include <iostream>
 
-/* namespace hersh { */
+namespace hersh {
 extern "C" {
     TSLanguage *tree_sitter_bash();
 }
 
+class TreeSitterTree {
+ public:
+  explicit TreeSitterTree(TSTree* tree) :
+    tree_(tree) {}
+  ~TreeSitterTree() {
+    ts_tree_delete(tree_);
+  }
+
+ private:
+  TSTree* tree_;
+
+};
+
 class TreeSitter {
  public:
   TreeSitter() {
-    std::cout << "In tree sitter" << std::endl;
-    TSLanguage *tree_sitter_bash();
 
-    TSParser *parser = ts_parser_new();
+    parser_ = ts_parser_new();
 
-    ts_parser_set_language(parser, tree_sitter_bash());
+    ts_parser_set_language(parser_, tree_sitter_bash());
 
-    std::string sourceCode = "ls -la";
 
-    TSTree *tree = ts_parser_parse_string(
-      parser,
-      NULL,
-      sourceCode.c_str(),
-      sourceCode.size()
-    );
+    /* TSNode root_node = ts_tree_root_node(tree); */
 
-    TSNode root_node = ts_tree_root_node(tree);
-
-    std::string string = ts_node_string(root_node);
-    std::cout << string << std::endl;
+    /* std::string string = ts_node_string(root_node); */
+    /* std::cout << string << std::endl; */
   }
+  ~TreeSitter() {
+    ts_parser_delete(parser_);
+  }
+
+  TreeSitterTree parse(const std::string& string) const {
+    return TreeSitterTree(ts_parser_parse_string(
+                          parser_,
+                          NULL,
+                          string.c_str(),
+                          string.size()
+                        )
+        );
+
+  }
+ private:
+  TSParser* parser_;
 };
+}
